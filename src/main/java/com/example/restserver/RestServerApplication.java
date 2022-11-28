f599d5a220a852adc8d46c974fab01bc0129a518
YunusGoven <yunuss.gvn@gmail.com>
2022-11-28 13:08:12 +0100

    add certificates

--- src/main/java/com/example/restserver/RestServerApplication.java
+++ src/main/java/com/example/restserver/RestServerApplication.java
@@ -3,11 +3,14 @@ package com.example.restserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RestServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestServerApplication.class, args);
     }
+
+
 
 }
